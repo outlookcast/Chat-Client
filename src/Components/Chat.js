9360e11299920b57c1fe4c9748879495c6966554
Vinicius Sampaio <viniciuscastrosampaio@gmail.com>
2018-12-28 16:38:59 -0200

    removing import

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Message from "./Message"
 import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import Dialog from '@material-ui/core/Dialog';
